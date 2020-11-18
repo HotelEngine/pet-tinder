cea165c1312ed9af897b3c19110910c1cdd951fa
Zach Romani <zjromani@gmail.com>
2020-11-18 14:40:45 -0700

    Add animal types

--- graphql/index.js
+++ graphql/index.js
@@ -1,18 +1,6 @@
+import animalType from './schema/types/animal'
+
 const { ApolloServer, gql } = require("apollo-server");
-
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-const typeDefs = gql`
-  type Pet {
-    name: String
-    type: String
-  }
-
-  type Query {
-    pets(latitude: Float, longitude: Float): [Pet]
-  }
-`;
 
 const { RESTDataSource } = require("apollo-datasource-rest");
 

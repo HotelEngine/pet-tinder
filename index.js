5105d014a3649dd5ed4749f929f106e0a360ad43
mdevoe12 <matthew.devoe@hotelengine.com>
2020-11-18 13:47:42 -0700

    updated 'getPets'

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const typeDefs = gql`
   }
 
   type Query {
-    pets: [Pet]
+    pets(latitude: Float, longitude: Float): [Pet]
   }
 `;
 
@@ -25,19 +25,20 @@ class PetFinder extends RESTDataSource {
     request.headers.set("Authorization", this.context.token);
   }
 
-  async getPets() {
-    const res = this.get(`v2/animals`);
+  async getPets(latitude, longitude) {
+    const res = await this.get(`v2/animals?location=${latitude},${longitude}`);
     console.log(JSON.stringify(res));
-    return res;
+    return res.animals;
   }
+
 }
 
 // Resolvers define the technique for fetching the types defined in the
 // schema. This resolver retrieves books from the "books" array above.
 const resolvers = {
   Query: {
-    pets: async (_source, _args, { dataSources }) => {
-      return dataSources.petFinderApi.getPets();
+    pets: async (_source, { latitude, longitude }, { dataSources }) => {
+      return dataSources.petFinderApi.getPets(latitude, longitude);
     },
   },
 };
@@ -55,7 +56,7 @@ const server = new ApolloServer({
   context: () => {
     return {
       token:
-        "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJHdUdRSmcyR09ZZ0p6NEozSlFNWnM3eEQxS0hEVFZTeVFiMFZBVnVabkdlWjlNTzVQSiIsImp0aSI6IjI4NmEyNGE2MTUxNGI2N2Q2NTRlMzRhYzVkY2FiNzU4YmMyYmM1NDExNzJlM2ZmMGUyYWVhYTk1ZWYwZjNmZDMzYTVjZDhkNjZiNDBhNTRiIiwiaWF0IjoxNjA1NzIxNDE3LCJuYmYiOjE2MDU3MjE0MTcsImV4cCI6MTYwNTcyNTAxNywic3ViIjoiIiwic2NvcGVzIjpbXX0.bXLyBDWxpWCwz8rHJzjzfygokSr5AJT7kG4trmHK7tEoRc3bFkhIRBsbHid6Itak6vVVY6Zk3vvnM-IdyLZEpETk0TMe2gZt5wDXhXBz4rifYmjM1yA4sAD69xmKINvuZXCYfnlQDEfycCqW61DBUEqqWbaw1aPnM4g-4YSJRL8lk5W0_uj4xz5cr0thvtQI1KwkuZONnepdRacTSNXa07Rg_WGkv1WghwtPlSfcbsRQdd3_zf9wNdXFMsVov6swDE9lp-7_-A-sLcMQ1vehTsnyj4D_1c85WxnecvRzTSyculFVdFKYaLes9TC7FAIphxMsPU-a5B5exKK1UTazZA",
+        "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJHdUdRSmcyR09ZZ0p6NEozSlFNWnM3eEQxS0hEVFZTeVFiMFZBVnVabkdlWjlNTzVQSiIsImp0aSI6IjdlMTM4NGU0ZjUwYWE2ZWUyMzU2NGYyM2VmYzU1N2ViNDY1ZGU1ZTM1YTgxNTM0ZjlhMzI5ODkxZDg3ZmI0OWQ2ZmYwOTZmY2VhZTIyYTdiIiwiaWF0IjoxNjA1NzI4NzI1LCJuYmYiOjE2MDU3Mjg3MjUsImV4cCI6MTYwNTczMjMyNSwic3ViIjoiIiwic2NvcGVzIjpbXX0.UHlsmwmL8yKDnsmugfJ6Ugs6hKONz4mucuPU8y6jSaJxt9Nbaq3UC_jE0h8Kpg0PDZKG_q4N3HokSSDKSYwd6duoILXQIm1wXnXRidJ3GddRISa3fPsttBBOBem5RLmd7AIRHYbg05RDbAP3-tLy9M8wzj9Fy6HPpMcJOJ_5rlKztW6N2TD__ujGtzI9jdPilMcxqCHq6CDBMG6wwkAu084tYjgP54MxN8kzURslvM-hsOK6EXQpns9hZVIaoBIynOEHY7b89Fec2Kk00_cndO6ieq1mQ7nkB_LQZHtRFtXvlY7_oQMOOm6Nr1IjR0KI3rHumk1C3mRWFgQWbYLSYw"
     };
   },
 });

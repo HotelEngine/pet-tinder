dbd7f359a388407be98c112d95716968457dc2ce
mdevoe12 <matthew.devoe@hotelengine.com>
2020-11-20 11:21:27 -0700

    updates

--- front-end/src/components/ProfileItem.tsx
+++ front-end/src/components/ProfileItem.tsx
@@ -4,48 +4,48 @@ import styles from '../assets/styles';
 import { Text, View } from 'react-native';
 import Icon from './Icon';
 
-const ProfileItem = ({ matches, name, age, description, distance }) => {
+const ProfileItem = ({ matches, name, age, description, distance, gender, size, status }) => {
     return (
         <View style={styles.containerProfileItem}>
             <View style={styles.matchesProfileItem}>
                 <Text style={styles.matchesTextProfileItem}>
-                    <Icon name="heart" /> {matches}% Match!
+                    <Icon name="heart" /> {matches.toFixed()}% Match!
                 </Text>
             </View>
 
             <Text style={styles.name}>{name}</Text>
 
-            <Text style={styles.descriptionProfileItem}>
-                {age} - {distance}
-            </Text>
+            <Text style={styles.descriptionProfileItem}>Age: {age}</Text>
+
+            <Text style={styles.descriptionProfileItem}>Distance: {distance.toFixed(1)} miles</Text>
 
             <View style={styles.info}>
                 <Text style={styles.iconProfile}>
-                    <Icon name="user" />
+                    <Icon name="circle" />
                 </Text>
                 <Text style={styles.infoContent}>{description}</Text>
             </View>
 
-            {/* <View style={styles.info}>
+            <View style={styles.info}>
                 <Text style={styles.iconProfile}>
                     <Icon name="circle" />
                 </Text>
-                <Text style={styles.infoContent}>{info2}</Text>
+                <Text style={styles.infoContent}>Gender: {gender}</Text>
             </View>
 
             <View style={styles.info}>
                 <Text style={styles.iconProfile}>
-                    <Icon name="hashtag" />
+                    <Icon name="circle" />
                 </Text>
-                <Text style={styles.infoContent}>{info3}</Text>
+                <Text style={styles.infoContent}>Size: {size}</Text>
             </View>
 
             <View style={styles.info}>
                 <Text style={styles.iconProfile}>
-                    <Icon name="calendar" />
+                    <Icon name="circle" />
                 </Text>
-                <Text style={styles.infoContent}>{info4}</Text>
-            </View> */}
+                <Text style={styles.infoContent}>Status: {status}</Text>
+            </View>
         </View>
     );
 };
